46c0eb1bf626e72a98093ca356626200ae7d4ded
1Huy2409 <nhathuy2409@gmail.com>
2024-11-17 16:37:52 +0700

    fix preview image

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -127,17 +127,17 @@ if (showAlert) {
 //     })
 // }
 
-// const uploadImage = document.querySelector("[upload-image]")
-// if (uploadImage) {
-//     //tim input va preview
-//     let uploadImageInput = document.querySelector("[upload-image-input]")
-//     let uploadImagePreview = document.querySelector("[upload-image-preview]")
-//     if (uploadImageInput) {
-//         uploadImageInput.addEventListener("change", (e) => {
-//             const file = e.target.files[0];
-//             if (file) [
-//                 uploadImagePreview.src = URL.createObjectURL(file)
-//             ]
-//         })
-//     }
-// }
+const uploadImage = document.querySelector("[upload-image]")
+if (uploadImage) {
+    //tim input va preview
+    let uploadImageInput = document.querySelector("[upload-image-input]")
+    let uploadImagePreview = document.querySelector("[upload-image-preview]")
+    if (uploadImageInput) {
+        uploadImageInput.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            if (file) [
+                uploadImagePreview.src = URL.createObjectURL(file)
+            ]
+        })
+    }
+}
--- routes/admin/products-category.route.js
+++ routes/admin/products-category.route.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const router = express.Router();
-const controller = require("../../controllers/admin/productsCategory.controller")
 const multer = require('multer')
-const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares")
+const router = express.Router();
 const upload = multer()
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares")
+const controller = require("../../controllers/admin/productsCategory.controller")
 //can tao mot validate khac cho trang tao moi danh muc san pham
 router.get('/', controller.index)
 router.get('/create', controller.create)
 router.post(
-    '/create', upload.single('thumbnail'),
+    '/create', 
+    upload.single('thumbnail'),
     uploadCloud.upload,
-    // validate.createPost, 
     controller.createPost
 )
 //form-change-multi route
